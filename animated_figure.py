b633e8b1ed10faab30deca3f1cb48ad2eed71647
SarinaMashreghi <sarina.m1384@gmail.com>
2024-01-20 18:24:37 -0500

    fake realtime

--- animated_figure.py
+++ animated_figure.py
@@ -1,49 +1,105 @@
 from dash import Dash, dcc, html, Input, Output
-#import plotly.express as px
-#import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+from datetime import datetime, timedelta
 
 
 ex_1 = pd.read_json("nbc_data/Exchange_1.json")
 ex_2 = pd.read_json("nbc_data/Exchange_2.json")
 ex_3 = pd.read_json("nbc_data/Exchange_3.json")
 
-print("1")
 total_df = pd.concat([ex_1, ex_2, ex_3])
 
-sym_1 = ex_1["Symbol"].to_list()
+start_time = datetime(2024, 1, 5, 9, 28)
+end_time = datetime(2024, 1, 5, 9, 32)
 
-print(sym_1)
+print(start_time+timedelta(0, 1))
 
-#fig = px.scatter(ex_1, y="OrderPrice", x="TimeStamp")
+sym_1 = ex_1["Symbol"].value_counts().keys().tolist()
+sym_2 = ex_2["Symbol"].value_counts().keys().tolist()
+sym_3 = ex_3["Symbol"].value_counts().keys().tolist()
 
-#fig.show()
+order_req_1 = ex_1.query("MessageType == 'NewOrderRequest'")
+order_ac_1 = ex_1.query("MessageType == 'NewOrderAcknowledged'")
+order_req_2 = ex_2.query("MessageType == 'NewOrderRequest'")
+order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
+order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
+order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
 
-#app = Dash(__name__)
+print("processing done")
 
+s = order_req_1.query(f"Symbol == '{sym_1[1]}'")
+# for symbol in sym_1:
+#     print(symbol)
+#     s = order_req_1.query(f"Symbol == '{symbol}'")
+#     for i in range(len(s)):
+#         #print(s.iloc[:6])
+#         fig = px.scatter(s, y="OrderPrice", x="TimeStampEpoch")
+#         fig.show()
+#     break
+
+# Base plot
+fig = go.Figure(
+    layout=go.Layout(
+        updatemenus=[dict(type="buttons", direction="right", x=0.9, y=1.16), ],
+        xaxis=dict(range=[start_time, end_time],
+                   autorange=False, tickwidth=2,
+                   title_text="Time"),
+        yaxis=dict(range=[0, 200],
+                   autorange=False,
+                   title_text="Price"),
+        title="Symbol1",
+    ))
+
+# Add traces
+init = 1
+
+fig.add_trace(
+    go.Scatter(x=s.TimeStampEpoch[:init],
+               y=s.OrderPrice[:init],
+               name="price",
+               line=None,
+               visible=True
+               ))
+
+
+
+td = 0.05
+curr_time = start_time
+fig.update(frames=[
+    go.Frame(
+        data=[go.Scatter(x=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["TimeStampEpoch"], 
+                         y=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["OrderPrice"], 
+                         line=None)]
+    )
+    for k in range(init, int(240//td))])
+
+fig.update_xaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=10)
+fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=1)
+fig.update_layout(yaxis_tickformat=',')
+fig.update_layout(legend=dict(x=0, y=1.1), legend_orientation="h")
+
+# Buttons
+fig.update_layout(
+    updatemenus=[
+        dict(
+            buttons=list([
+                dict(label="Play",
+                        method="animate",
+                    args=[None, {"frame": {"duration": td*1000}}])
+            ]))])
+
+fig.show()
+
+# app = Dash(__name__)
 
 # app.layout = html.Div([
-#     html.H4('Animated GDP and population over decades'),
-#     #html.P("Select an animation:"),
-#     # dcc.RadioItems(
-#     #     id='selection',
-#     #     options=["GDP - Scatter", "Population - Bar"],
-#     #     value='GDP - Scatter',
-#     # ),
-#     dcc.Loading(dcc.Graph(id="graph"), type="cube")
+#     dcc.Graph(
+#         id='life-exp-vs-gdp',
+#         figure=fig
+#     )
 # ])
-
-
-# @app.callback(
-#     Output("graph"),
-#     Input())
-# def display_animated_graph():
-#     #df = px.data.gapminder()  # replace with your own data source
-#     ex_1 = pd.read_json("nbc_data/Exchange_1.json")
-#     animations = {
-#         'Price scatter': px.scatter(ex_1, x="OrderPrice", animation_frame="TimeStamp")
-#     }
-#     return animations['Price scatter']
 
 
 # app.run_server(debug=True)
