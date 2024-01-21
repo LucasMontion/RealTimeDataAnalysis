5777bf2b62ce2af2eae4a563dd565c8a29344b08
SarinaMashreghi <sarina.m1384@gmail.com>
2024-01-21 00:44:41 -0500

    more features

--- app.py
+++ app.py
@@ -16,12 +16,6 @@ sym_1 = ex_1["Symbol"].value_counts().keys().tolist()
 sym_2 = ex_2["Symbol"].value_counts().keys().tolist()
 sym_3 = ex_3["Symbol"].value_counts().keys().tolist()
 
-order_req_1 = ex_1.query("MessageType == 'NewOrderRequest'")
-order_ac_1 = ex_1.query("MessageType == 'NewOrderAcknowledged'")
-order_req_2 = ex_2.query("MessageType == 'NewOrderRequest'")
-order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
-order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
-order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
 
 order_req_1 = ex_1.query("MessageType == 'NewOrderRequest'")
 order_ac_1 = ex_1.query("MessageType == 'NewOrderAcknowledged'")
@@ -29,18 +23,32 @@ order_req_2 = ex_2.query("MessageType == 'NewOrderRequest'")
 order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
 order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
 order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
+
+cancel_1 = ex_1.query("MessageType == 'Cancelled'")
+cancel_2 = ex_2.query("MessageType == 'Cancelled'")
+cancel_3 = ex_3.query("MessageType == 'Cancelled'")
+
+
+order_dict = {'1': order_req_1, '2':order_req_2, '3': order_req_3}
+sym_dict = {'1': sym_1, '2': sym_2, '3': sym_3}
+cancel_dict = {'1': cancel_1, '2': cancel_2, '3': cancel_3}
 
 sample_price = order_req_1[order_req_1["Symbol"] == sym_1[0]]
 
 print(sample_price.head().to_json())
 
-#sample_price['MA'] = sample_price["OrderPrice"].rolling(window=5).mean() # moving average
+sample_price['MA'] = sample_price["OrderPrice"].rolling(window=5).mean() # moving average
+print(sample_price['MA'].head(10))
 
 
 app = Flask(__name__)
 
 CORS(app)
-@app.route('/')
+@app.route('/get_symbols/<exchange>')
+def get_symbols(exchange):
+    return sym_dict[exchange]
+
+@app.route('/set_time/')
 def start_server():
     start = 1704464880 # 01/05/2024
     global delta
@@ -48,16 +56,33 @@ def start_server():
     print("delta ", delta)
     return str(delta)
 
-
-@app.route('/<prev_time>')
-def upload_data(prev_time):
+@app.route('/price_data/<exchange>/<symbol>/<prev_time>')
+def upload_data(exchange, symbol, prev_time):
     global delta
+    global volume
     prev_time = datetime.datetime.fromtimestamp(int(prev_time))
     curr_time = datetime.datetime.fromtimestamp(int(time.time()) - delta)
-    s = sample_price.query(f"TimeStampEpoch > '{prev_time}' and TimeStampEpoch <= '{curr_time}'")
-    return [s["TimeStampEpoch"].to_list(), s["OrderPrice"].to_list()]
+    s = order_dict[exchange].query(f"Symbol == '{symbol}'").query(f"TimeStampEpoch > '{prev_time}' and TimeStampEpoch <= '{curr_time}'")
+    print("here")
+    d = cancel_dict[exchange].query(f"Symbol == '{symbol}'").query(f"TimeStampEpoch > '{prev_time}' and TimeStampEpoch <= '{curr_time}'")
+    
+                             
+    volume.update(set(s["OrderID"]))
+    
+    
+    for id in volume:
+        if not id in set(d["OrderID"]):
+            print(id)
+            
+    volume -= set(d["OrderID"])
+    #print(set(s["OrderID"]), set(d["OrderID"]))
+    
+    return [s["TimeStampEpoch"].to_list(), s["OrderPrice"].to_list(), len(volume)]
+
 
 if __name__ == '__main__':
     global delta
+    global volume
+    volume = set()
     app.debug = True
     app.run()
