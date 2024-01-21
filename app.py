630aa8c21a32de47fce9fd20e9322bf43d528111
SarinaMashreghi <sarina.m1384@gmail.com>
2024-01-20 22:23:17 -0500

    app change

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import pandas as pd
+import polars as pl
 import datetime
 
 import random
@@ -22,11 +23,32 @@ order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
 order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
 order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
 
-sample_price = order_req_1[order_req_1["Symbol"] == sym_1[0]][["OrderPrice", "TimeStampEpoch"]]
+order_req_1 = ex_1.query("MessageType == 'NewOrderRequest'")
+order_ac_1 = ex_1.query("MessageType == 'NewOrderAcknowledged'")
+order_req_2 = ex_2.query("MessageType == 'NewOrderRequest'")
+order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
+order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
+order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
+
+sample_price = order_req_1[order_req_1["Symbol"] == sym_1[0]]
+
+sample_price['MA'] = sample_price["OrderPrice"].rolling(window=5).mean() # moving average
+
+global start 
+global delta
 
 app = Flask(__name__)
 
 CORS(app)
-@app.route('/')
-def upload_data(currtime):
-    return sample_price.query(f"TimeStampEpoch <= '{currtime}'").to_dict()    
+@app.route('/<start_time>')
+def start_server(start_time):
+    start = start_time
+    delta = datetime.datetime.now() - start
+    return start
+
+
+
+@app.route('/<prev_time>')
+def upload_data(prev_time):
+    curr_time = datetime.datetime.now() - delta
+    return sample_price.query(f"{prev_time}<TimeStampEpoch <= {curr_time}").to_dict()    
