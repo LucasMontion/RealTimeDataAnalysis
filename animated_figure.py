824540f67be3254eab3a8b580e1c9450b4ac104e
SarinaMashreghi <sarina.m1384@gmail.com>
2024-01-21 06:38:53 -0500

    prettier graphs

--- animated_figure.py
+++ animated_figure.py
@@ -1,6 +1,7 @@
 from dash import Dash, dcc, html, Input, Output, callback, State
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
 from datetime import datetime, timedelta
 import numpy as np
@@ -10,15 +11,35 @@ ex_1 = pd.read_json("nbc_data/Exchange_1.json")
 ex_2 = pd.read_json("nbc_data/Exchange_2.json")
 ex_3 = pd.read_json("nbc_data/Exchange_3.json")
 
+cancel_1 = ex_1.query("MessageType == 'Cancelled'")
+cancel_2 = ex_2.query("MessageType == 'Cancelled'")
+cancel_3 = ex_3.query("MessageType == 'Cancelled'")
+
 total_df = pd.concat([ex_1, ex_2, ex_3])
 
-start_time = datetime(2024, 1, 5, 9, 29, 59)
+sym_1 = ex_1["Symbol"].value_counts().keys().tolist()
+sym_2 = ex_2["Symbol"].value_counts().keys().tolist()
+sym_3 = ex_3["Symbol"].value_counts().keys().tolist()
+
+
+order_req_1 = ex_1.query("MessageType == 'NewOrderRequest'")
+order_ac_1 = ex_1.query("MessageType == 'NewOrderAcknowledged'")
+order_req_2 = ex_2.query("MessageType == 'NewOrderRequest'")
+order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
+order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
+order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
+
+order_dict = {'1': order_req_1, '2':order_req_2, '3': order_req_3}
+sym_dict = {'1': sym_1, '2': sym_2, '3': sym_3}
+cancel_dict = {'1': cancel_1, '2': cancel_2, '3': cancel_3}
+
+total_df = pd.concat([ex_1, ex_2, ex_3])
+
+start_time = datetime(2024, 1, 5, 9, 28)
 end_time = datetime(2024, 1, 5, 9, 32)
 
 global curr_time 
-curr_time = datetime(2024, 1, 5, 9, 29, 59)
-
-print(start_time+timedelta(0, 1))
+curr_time = start_time
 
 sym_1 = ex_1["Symbol"].value_counts().keys().tolist()
 sym_2 = ex_2["Symbol"].value_counts().keys().tolist()
@@ -34,96 +55,127 @@ order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
 print("processing done")
 
 s = order_req_1.query(f"Symbol == '{sym_1[0]}'")
-print(len(s))
-# for symbol in sym_1:
-#     print(symbol)
-#     s = order_req_1.query(f"Symbol == '{symbol}'")
-#     for i in range(len(s)):
-#         #print(s.iloc[:6])
-#         fig = px.scatter(s, y="OrderPrice", x="TimeStampEpoch")
-#         fig.show()
-#     break
 
-# Base plot
-# fig = go.Figure(
-#     layout=go.Layout(
-#         updatemenus=[dict(type="buttons", direction="right", x=0.9, y=1.16), ],
-#         xaxis=dict(range=[start_time, end_time],
-#                    autorange=False, tickwidth=2,
-#                    title_text="Time"),
-#         yaxis=dict(range=[0, s.OrderPrice.max()],
-#                    autorange=False,
-#                    title_text="Price"),
-#         title="Symbol1",
-#     ))
-
-# Add traces
-init = 1
-
-# fig.add_trace(
-#     go.Scatter(x=s.TimeStampEpoch[:init],
-#                y=s.OrderPrice[:init],
-#                name="price",
-#                line=None,
-#                visible=True
-#                ))
-
-
-
-# td = 1
-# curr_time = start_time
-# fig.update(frames=[
-#     go.Frame(
-#         data=[go.Scatter(x=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["TimeStampEpoch"], 
-#                          y=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["OrderPrice"], 
-#                          line=None)]
-#     )
-#     for k in range(init, int(240//td))])
-
-# fig.update_xaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=10)
-# fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=1)
-# fig.update_layout(yaxis_tickformat=',')
-# fig.update_layout(legend=dict(x=0, y=1.1), legend_orientation="h")
-
-# # Buttons
-# fig.update_layout(
-#     updatemenus=[
-#         dict(
-#             buttons=list([
-#                 dict(label="Play",
-#                         method="animate",
-#                     args=[None, {"frame": {"duration": td*1000}}])
-#             ]))])
-
-#fig.show()
-td = 75
+global td
+td = 200
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='graph'),
     dcc.Interval(id='interval', interval=td, max_intervals=-1),
-    dcc.Store(id='index', data=0),
-    dcc.Store(id='curr_time', data=start_time)
+    html.H1("Price vs Time"),
+    html.Div([
+        dcc.Dropdown(sym_1, sym_1[0], id='symbol1'),
+        dcc.Graph(
+            id='graph1'),
+        
+        dcc.Store(id='index1', data=0),
+    ]),
+    html.Div([
+        dcc.Dropdown(sym_2, sym_2[0], id='symbol2'),
+        dcc.Graph(
+            id='graph2'),
+        
+        dcc.Store(id='index2', data=0),
+    ]),
+    html.Div([
+        dcc.Dropdown(sym_3, sym_3[0], id='symbol3'),
+        dcc.Graph(
+            id='graph3'),
+        
+        dcc.Store(id='index3', data=0),
+    ]),
+    html.Div([
+        dcc.RadioItems(
+            id='exchange',
+            options=['Exchange 1', 'Exchange 2', 'Exchange 3'],
+            value='Exchange 1',
+        ),
+        dcc.Loading(dcc.Graph(id="bubble_graph"), type="cube")
+    ])
 ])
 
 @callback(
-    Output('graph', 'figure'),
-    Output('index', 'data'),
-    #Output('curr_time', 'data'),
+    Output('graph1', 'figure'),
+    Output('index1', 'data'),
     Input('interval', 'n_intervals'),
-    State('index', 'data')
-    #State('curr_time', 'data')
+    Input('symbol1', 'value'),
+    State('index1', 'data')
     )
-def update(n_intervals, ind):
-    #filtered_df = s[s["TimeStampEpoch"] <= start_time+timedelta(milliseconds=n_intervals*td)]
-    curr_time = start_time + timedelta(milliseconds=td*n_intervals)
-    #print(curr_time)
-    while(s.iloc[ind].TimeStampEpoch < curr_time):
+def update_1(n_intervals, symbol, ind):
+    s = order_req_1.query(f"Symbol == '{symbol}'")
+    global curr_time
+    curr_time = curr_time + timedelta(milliseconds=200)
+    while(ind < len(s) and s.iloc[ind].TimeStampEpoch < curr_time):
         ind += 1
-    fig = px.scatter(s[:ind], x="TimeStampEpoch", y="OrderPrice", range_x=[start_time, end_time], title = str(curr_time))
+    s["MA"] = s["OrderPrice"].rolling(5).mean()
+    fig = make_subplots(rows=2, cols=1)
+    fig.append_trace(go.Scatter(x=s[:ind]["TimeStampEpoch"], y=s[:ind]["OrderPrice"]), row=1, col=1)
+    fig.append_trace(go.Scatter(x=s[:ind]["TimeStampEpoch"], y=s[:ind]["MA"]), row=2, col=1)
 
     return fig, ind
 
+@callback(
+    Output('graph2', 'figure'),
+    Output('index2', 'data'),
+    Input('interval', 'n_intervals'),
+    Input('symbol2', 'value'),
+    State('index2', 'data')
+    )
+def update_2(n_intervals, symbol, ind):
+    s = order_req_2.query(f"Symbol == '{symbol}'")
+    global curr_time
+    curr_time = curr_time + timedelta(milliseconds=200)
+    
+    s["MA"] = s["OrderPrice"].rolling(5).mean()
+    while(ind < len(s) and s.iloc[ind].TimeStampEpoch < curr_time):
+        ind += 1
+    fig = px.scatter(s[:ind], x="TimeStampEpoch", y="OrderPrice", range_x=[start_time, end_time], title = str(curr_time),
+                     labels = {"OrderPrice":"Price", "TimeStampEpoch":"Time"})
+    #fig = px.plot(s[:ind], x="TimeStampEpoch", y="OrderPrice", range_x=[start_time, end_time])
+
+    return fig, ind
+
+@callback(
+    Output('graph3', 'figure'),
+    Output('index3', 'data'),
+    Input('interval', 'n_intervals'),
+    Input('symbol3', 'value'),
+    State('index3', 'data')
+    )
+def update_3(n_intervals, symbol, ind):
+    s = order_req_3.query(f"Symbol == '{symbol}'")
+    global curr_time
+    curr_time = curr_time + timedelta(milliseconds=200)    
+    while(ind < len(s)  and s.iloc[ind].TimeStampEpoch < curr_time):
+        ind += 1
+    fig = px.scatter(s[:ind], x="TimeStampEpoch", y="OrderPrice", range_x=[start_time, end_time], title = str(curr_time),
+                     labels = {"OrderPrice":"Price", "TimeStampEpoch":"Time"})
+
+    return fig, ind
+
+@callback(
+    Output('bubble_graph', 'figure'), 
+    Input('exchange', 'value')
+)
+def bubble(ex):
+    df = order_dict[ex[-1]]
+    print(df.head())
+    df = df.copy()
+    df = df.sort_values(by=['Symbol']).reset_index(drop=True)
+    df['std'] = df.groupby('Symbol')['OrderPrice'].expanding().std().reset_index(drop=True)
+    df['max_values'] = df.groupby('Symbol')['OrderPrice'].expanding().max().reset_index(drop=True)
+    df['min_values'] = df.groupby('Symbol')['OrderPrice'].expanding().min().reset_index(drop=True)
+    df['max_min_difference'] = df['max_values'] - df['min_values']
+    df['mean_price'] = df.groupby('Symbol')['OrderPrice'].expanding().mean().reset_index(drop=True)
+    
+    print(df.head())
+
+    animations = {
+        'Variance': px.scatter(df, x = "max_min_difference", y = "std", animation_group="Symbol", size = 'mean_price', 
+                               animation_frame = "TimeStampEpoch", hover_name = "Symbol", log_x = True 
+                               )
+    }
+    
+    return animations
 
 app.run_server(debug=True)
