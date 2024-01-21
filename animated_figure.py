b1de9df4983eb4b99f6e3cac446aa7426c763ddf
SarinaMashreghi <sarina.m1384@gmail.com>
2024-01-20 19:47:39 -0500

    better realtime

--- animated_figure.py
+++ animated_figure.py
@@ -1,8 +1,9 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime, timedelta
+import numpy as np
 
 
 ex_1 = pd.read_json("nbc_data/Exchange_1.json")
@@ -11,7 +12,7 @@ ex_3 = pd.read_json("nbc_data/Exchange_3.json")
 
 total_df = pd.concat([ex_1, ex_2, ex_3])
 
-start_time = datetime(2024, 1, 5, 9, 28)
+start_time = datetime(2024, 1, 5, 9, 29, 59)
 end_time = datetime(2024, 1, 5, 9, 32)
 
 print(start_time+timedelta(0, 1))
@@ -29,7 +30,8 @@ order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
 
 print("processing done")
 
-s = order_req_1.query(f"Symbol == '{sym_1[1]}'")
+s = order_req_1.query(f"Symbol == '{sym_1[0]}'")
+print(len(s))
 # for symbol in sym_1:
 #     print(symbol)
 #     s = order_req_1.query(f"Symbol == '{symbol}'")
@@ -40,66 +42,75 @@ s = order_req_1.query(f"Symbol == '{sym_1[1]}'")
 #     break
 
 # Base plot
-fig = go.Figure(
-    layout=go.Layout(
-        updatemenus=[dict(type="buttons", direction="right", x=0.9, y=1.16), ],
-        xaxis=dict(range=[start_time, end_time],
-                   autorange=False, tickwidth=2,
-                   title_text="Time"),
-        yaxis=dict(range=[0, 200],
-                   autorange=False,
-                   title_text="Price"),
-        title="Symbol1",
-    ))
+# fig = go.Figure(
+#     layout=go.Layout(
+#         updatemenus=[dict(type="buttons", direction="right", x=0.9, y=1.16), ],
+#         xaxis=dict(range=[start_time, end_time],
+#                    autorange=False, tickwidth=2,
+#                    title_text="Time"),
+#         yaxis=dict(range=[0, s.OrderPrice.max()],
+#                    autorange=False,
+#                    title_text="Price"),
+#         title="Symbol1",
+#     ))
 
 # Add traces
 init = 1
 
-fig.add_trace(
-    go.Scatter(x=s.TimeStampEpoch[:init],
-               y=s.OrderPrice[:init],
-               name="price",
-               line=None,
-               visible=True
-               ))
+# fig.add_trace(
+#     go.Scatter(x=s.TimeStampEpoch[:init],
+#                y=s.OrderPrice[:init],
+#                name="price",
+#                line=None,
+#                visible=True
+#                ))
 
 
 
-td = 0.05
-curr_time = start_time
-fig.update(frames=[
-    go.Frame(
-        data=[go.Scatter(x=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["TimeStampEpoch"], 
-                         y=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["OrderPrice"], 
-                         line=None)]
-    )
-    for k in range(init, int(240//td))])
-
-fig.update_xaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=10)
-fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=1)
-fig.update_layout(yaxis_tickformat=',')
-fig.update_layout(legend=dict(x=0, y=1.1), legend_orientation="h")
-
-# Buttons
-fig.update_layout(
-    updatemenus=[
-        dict(
-            buttons=list([
-                dict(label="Play",
-                        method="animate",
-                    args=[None, {"frame": {"duration": td*1000}}])
-            ]))])
-
-fig.show()
-
-# app = Dash(__name__)
-
-# app.layout = html.Div([
-#     dcc.Graph(
-#         id='life-exp-vs-gdp',
-#         figure=fig
+# td = 1
+# curr_time = start_time
+# fig.update(frames=[
+#     go.Frame(
+#         data=[go.Scatter(x=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["TimeStampEpoch"], 
+#                          y=s.query(f"TimeStampEpoch < '{start_time + timedelta(0, k*td)}'")["OrderPrice"], 
+#                          line=None)]
 #     )
-# ])
+#     for k in range(init, int(240//td))])
+
+# fig.update_xaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=10)
+# fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=1)
+# fig.update_layout(yaxis_tickformat=',')
+# fig.update_layout(legend=dict(x=0, y=1.1), legend_orientation="h")
+
+# # Buttons
+# fig.update_layout(
+#     updatemenus=[
+#         dict(
+#             buttons=list([
+#                 dict(label="Play",
+#                         method="animate",
+#                     args=[None, {"frame": {"duration": td*1000}}])
+#             ]))])
+
+#fig.show()
+td = 100
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='graph'),
+    dcc.Interval(id='interval', interval=td, max_intervals=-1)
+])
+
+@callback(
+    Output('graph', 'figure'),
+    Input('interval', 'n_intervals'))
+def update(n_intervals):
+    filtered_df = s[s["TimeStampEpoch"] <= start_time+timedelta(milliseconds=n_intervals*td)]
+    fig = px.scatter(filtered_df, x="TimeStampEpoch", y="OrderPrice", range_x=[start_time, end_time], title = str(start_time+timedelta(milliseconds=n_intervals*td)))
+    if(n_intervals==len(s)):
+        print(n_intervals)
+    return fig
 
 
-# app.run_server(debug=True)
+app.run_server(debug=True)
