9abd0cb834e90de768e5303feb704a864bf5251a
Lucas Montion <lucasmontion@gmail.com>
2024-07-07 01:18:26 -0400

    Backend Works!
    Gets data in range of time (500,100,10 ms)

--- app.py
+++ app.py
@@ -1,10 +1,186 @@
 from flask import Flask
 from flask_cors import CORS
+import pandas as pd
+import datetime
+import time
+from datetime import timedelta
+
 
 import random
+
+ex_1 = pd.read_json("nbc_data/Exchange_1.json")
+ex_2 = pd.read_json("nbc_data/Exchange_2.json")
+ex_3 = pd.read_json("nbc_data/Exchange_3.json")
+
+total_df = pd.concat([ex_1, ex_2, ex_3])
+
+sym_1 = ex_1["Symbol"].value_counts().keys().tolist()
+sym_2 = ex_2["Symbol"].value_counts().keys().tolist()
+sym_3 = ex_3["Symbol"].value_counts().keys().tolist()
+
+
+order_req_1 = ex_1.query("MessageType == 'NewOrderRequest'")
+order_ac_1 = ex_1.query("MessageType == 'NewOrderAcknowledged'")
+order_req_2 = ex_2.query("MessageType == 'NewOrderRequest'")
+order_ac_2 = ex_2.query("MessageType == 'NewOrderAcknowledged'")
+order_req_3 = ex_3.query("MessageType == 'NewOrderRequest'")
+order_ac_3 = ex_3.query("MessageType == 'NewOrderAcknowledged'")
+
+cancel_1 = ex_1.query("MessageType == 'Cancelled'")
+cancel_2 = ex_2.query("MessageType == 'Cancelled'")
+cancel_3 = ex_3.query("MessageType == 'Cancelled'")
+
+
+merged_orders_df = pd.concat([order_req_1,order_req_2,order_req_3], axis=0)
+merged_df_sorted = merged_orders_df.sort_values(by='TimeStamp')
+orders_df = merged_df_sorted.reset_index(drop=True)
+
+
+order_dict = {'1': order_req_1, '2':order_req_2, '3':order_req_3}
+sym_dict = {'1': sym_1, '2': sym_2, '3': sym_3}
+cancel_dict = {'1': cancel_1, '2': cancel_2, '3': cancel_3}
+
+
+# - Global Variables - #
+index_symb = 0
+temp_index_symb = 0
+symb_temp = 'PRQ83'
+time_start = next(iter(orders_df['TimeStamp'])).replace(second=0, microsecond=0)
+symb_start_time = next(iter(orders_df['TimeStamp'])).replace(second=0, microsecond=0)
+symb_period_end = next(iter(orders_df['TimeStamp'])).replace(second=0, microsecond=0)
+
 app = Flask(__name__)
+
 
 CORS(app)
 @app.route('/')
-def hello_world():
-    return str(random.randint(1, 10))
+def printInfo():
+    print('Basic route')
+   
+
+@app.route('/get/<symbol>')
+def get_symbol(symbol):
+    global index_symb
+    global time_start
+    global symb_start_time
+    global symb_period_end
+    global symb_temp
+    global temp_index_symb
+
+    if (symb_temp == ''):
+        symb_temp = symbol
+        index_symb = 0
+    elif (symb_temp != '' and symb_temp != symbol):
+        symb_temp = symbol
+        index_symb = 0
+        symb_start_time == next(iter(orders_df['TimeStamp']))
+    
+
+    mask = orders_df['Symbol'] == symb_temp
+    filtered_orders_df = orders_df[mask]
+    filt_reset_orders_df = filtered_orders_df.reset_index(drop=True)
+
+    # - Start time of the specified symbol - #
+    
+    if symb_start_time == next(iter(orders_df['TimeStamp'])):
+        symb_start_time = next(iter(filt_reset_orders_df['TimeStamp']))
+        delta = datetime.timedelta(milliseconds=10)
+        symb_period_end = symb_start_time + delta
+        print('1')
+    elif temp_index_symb == index_symb:
+        symb_start_time = symb_period_end
+        delta = datetime.timedelta(milliseconds=10)
+        symb_period_end = symb_start_time + delta
+        print('2')
+    
+
+    #print('symb time')
+    #print(filt_reset_orders_df['TimeStamp'][index_symb])
+    #print('time start')
+    #print(symb_start_time)
+    #print('time end')
+    #print(symb_period_end)
+    #print(symb_period_end > filt_reset_orders_df['TimeStamp'][index_symb] >= symb_start_time)
+    
+    in_range_timestamp = []
+    inrange = True
+    #for k, v in filt_reset_orders_df.items():
+    #    for i in range(len(filt_reset_orders_df['TimeStamp'])):
+    #        if k == 'TimeStamp' and symb_start_time <= v[i] <= symb_period_end:
+    #            in_range_timestamp.append(v[i])
+    #            #this is only timestamp need to add orderprice
+    #        else:
+    #            inrange = False
+
+    for i in range(len(filt_reset_orders_df['TimeStamp'])):
+        timestamp = filt_reset_orders_df['TimeStamp'][i]
+        if symb_start_time <= timestamp <= symb_period_end:
+            order_price = filt_reset_orders_df['OrderPrice'][i]
+            in_range_timestamp.append((timestamp, order_price))
+
+
+
+    if inrange:
+        temp_index_symb = index_symb
+        print('inrange')
+    else:
+        index_symb += 1
+        print('outrange')
+
+    #if index_symb < len(filt_reset_orders_df) and (symb_period_end > filt_reset_orders_df['TimeStamp'][index_symb] >= symb_start_time):
+    #    result = str(filt_reset_orders_df['OrderPrice'][index_symb])
+    #    index_symb += 1
+    #    print('3')
+    #else :
+    #    temp_index_symb = index_symb
+    #    result = ''
+    #    print('4')
+
+    print(in_range_timestamp)
+    return in_range_timestamp
+
+@app.route('/get_symbols/<exchange>')
+def get_symbols(exchange):
+    return sym_dict[exchange]
+
+@app.route('/set_time/')
+def start_server():
+    start = 1704464880 # 01/05/2024
+    global delta
+    delta = int(time.time()) - start
+    print("delta ", delta)
+    return str(start)
+
+@app.route('/price_data/<exchange>/<symbol>/<prev_time>')
+def upload_data(exchange, symbol, prev_time):
+    global delta
+    global volume
+    prev_time = datetime.datetime.fromtimestamp(int(prev_time))
+    curr_time = datetime.datetime.fromtimestamp(int(time.time()) - delta)
+    s = order_dict[exchange].query(f"Symbol == '{symbol}'").query(f"TimeStampEpoch > '{prev_time}' and TimeStampEpoch <= '{curr_time}'")
+    print("here")
+    d = cancel_dict[exchange].query(f"Symbol == '{symbol}'").query(f"TimeStampEpoch > '{prev_time}' and TimeStampEpoch <= '{curr_time}'")
+    
+                             
+    volume.update(set(s["OrderID"]))
+    
+    
+    for id in volume:
+        if not id in set(d["OrderID"]):
+            print(id)
+            
+    volume -= set(d["OrderID"])
+    #print(set(s["OrderID"]), set(d["OrderID"]))
+    
+    return [s["TimeStampEpoch"].to_list(), s["OrderPrice"].to_list(), len(volume)]
+
+# @app.route('/stat_data/')
+# def get_data():
+
+if __name__ == '__main__':
+    global delta
+    global volume
+    volume = set()
+    app.debug = True
+    app.run()
+    
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,7 @@ import Plot from 'react-plotly.js';
 import axios from 'axios';
 import Plotly from 'plotly.js-dist';
 
-async function getNumber() {
-  try {
-    const response = await axios.get('http://127.0.0.1:5000/');
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-}
+
 
 function App() {
   const [y, setY] = useState([1]);
@@ -26,33 +18,71 @@ function App() {
 
   const ms = 10;
 
+  const [symbol, setSymbol] = useState('PRQ83')
+  const [symbolList, setSymbolList] = useState([])
+  let handleSymbolChange = (e) => {
+    setSymbol(e.target.value)
+  }
   useEffect(() => {
+    async function getNumber() {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/get/'+symbol);
+      
+        console.log(response.data);
+        return response.data;
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    }
 
+    const fetchData = async () => {
+      try {
+        // Example JSON file URLs
+        const urls = [
+          'nbc_data/Exchange_1.json',
+          'nbc_data/Exchange_2.json',
+          'nbc_data/Exchange_3.json'
+        ];
+
+        // Fetch data from each URL
+        const responses = await Promise.all(urls.map(url => fetch(url)));
+        const jsons = await Promise.all(responses.map(response => response.json()));
+
+        // Extract symbols from each JSON file
+        const allSymbols = jsons.flatMap(json => json.map(entry => entry.Symbol)); // Assuming each entry has a 'Symbol' field
+
+        // Update state with unique symbols
+        setSymbolList(allSymbols);
+        console.log(allSymbols);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    }
+    
     const intervalId = setInterval(async () => {
-      if(avrg.length === 1 || avrg.length === 0) {
+      if (avrg.length === 1 || avrg.length === 0) {
         setData1(1);
       }
       let num = await getNumber();
-      let num2 = await getNumber();
-
-        setY((prevY) => [...prevY, num]); // Append a random number between 0 and 10
-        setX((prevX) => [...prevX, prevX[prevX.length - 1] + 1])
-     
-       // setY((prevY) => [...prevY, num2]); // Append a random number between 0 and 10
-       // setX((prevX) => [...prevX, prevX[prevX.length - 1] + 1])
+      if( num != 'NaN') {
+        setY((prevY) => [...prevY, num]); // Append the order price of the symbol
+        setX((prevX) => [...prevX, prevX[prevX.length - 1] + 1]) // Move to next point on the chart
+      }
       
-
-      if (ms === 10 && x[x.length - 1] + 1 % 10) {
+      /*if (ms === 10 && x[x.length - 1] + 1 % 10) {
         setAvrg((prevAvrg) => [...prevAvrg, (data1) / ((x[x.length - 1] + 1) / 2.5)]);
       } else if (ms === 100) {
         setAvrg((prevAvrg) => [...prevAvrg, ((data1) / (x[x.length - 1] + 1))]); // Calculate average
-      }
+      }*/
 
       setData1((data1 + num));
       setAnimationFrame(prevFrame => prevFrame + 1);
-
+      
     }, ms); // Adjust the interval (in milliseconds) according to your needs
 
+    fetchData();
+    
     return () => clearInterval(intervalId); // Cleanup the interval on component unmount
   }, [y, avrg, data1, x]);
 
@@ -73,7 +103,9 @@ function App() {
     <>
       <div style={{ display: 'block', flexDirection: 'column', backgroundColor: '#212121', margin: 0 }}>
         <h1 style={{ color: 'white', fontFamily: 'verdana', textAlign: 'center', paddingTop: 15, margin: 0, height: 70 }}>ConU24</h1>
-
+        <select onChange={handleSymbolChange}>
+          {symbolList.map((sym) => <option value={sym}>{sym}</option>)}
+        </select>
         <div style={{ display: 'flex' }}>
 
           <Plot
@@ -84,7 +116,7 @@ function App() {
                 type: 'scatter',
                 mode: 'lines+markers',
                 name: 'Symbol',
-                marker: { color: 'lightgrey' },
+                marker: { color: 'white' },
                 visible: showPrice ? 'true' : 'legendonly',
               },
               {
@@ -95,6 +127,10 @@ function App() {
                 mode: 'lines',
                 name: 'Average',
                 marker: { color: 'red' },
+                line: {
+                  dash: 'dot',
+                  width: 4
+                }
               },
             ]}
             layout={{
@@ -105,8 +141,8 @@ function App() {
                 mirror: true,
                 ticks: 'outside',
                 showline: true,
-                linecolor: 'lightgrey',
-                gridcolor: 'lightgrey',
+                linecolor: 'black',
+                gridcolor: 'black',
                 color: 'white',
               },
               yaxis: {
@@ -114,8 +150,8 @@ function App() {
                 mirror: true,
                 ticks: 'outside',
                 showline: true,
-                linecolor: 'lightgrey',
-                gridcolor: 'lightgrey',
+                linecolor: 'black',
+                gridcolor: 'black',
                 color: 'white',
               },
               showlegend: false
@@ -128,7 +164,7 @@ function App() {
                 checked={showPrice}
                 onChange={() => { togglePriceVisibility() }}
               />
-              <p style={{ color: 'white', fontFamily: 'verdana' }}>Current: {y[y.length - 1].toFixed(2)}</p>
+              <p style={{ color: 'white', fontFamily: 'verdana' }}>Current: {y[y.length - 1]}</p>
             </div>
             <div style={{ display: 'flex' }}>
               <input
@@ -136,7 +172,7 @@ function App() {
                 checked={showAverage}
                 onChange={() => { toggleAverageVisibility() }}
               />
-              <p style={{ color: 'red', fontFamily: 'verdana' }}>Average: {avrg[avrg.length - 1].toFixed(2)}</p>
+              <p style={{ color: 'red', fontFamily: 'verdana' }}>Average: {avrg[avrg.length - 1].toFixed(4)}</p>
             </div>
 
           </div>
